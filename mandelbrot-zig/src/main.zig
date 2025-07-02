3db555f66858586aa97620da7a748e5b1b5b43c1
AgentE382 <AgentE382@users.noreply.github.com>
2025-07-02 01:11:31 -0400

    mandelbrot: remove boilerplate

--- mandelbrot-zig/src/main.zig
+++ mandelbrot-zig/src/main.zig
@@ -1,6 +1,3 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
 const zigimg = @import("zigimg");
 
@@ -222,22 +219,4 @@ fn writeImage(filename: []const u8, pixels: []const u8, imgSize: [2]usize) !void
     defer image.deinit();
 
     try image.writeToFilePath(filename, zigimg.Image.EncoderOptions{ .png = .{} });
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
